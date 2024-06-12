c1e9bf358c8145aa80ca15223a60c048895e484c
Parvathaneni Tagur <parvathaneni.tagur@gmail.com>
2024-06-12 16:07:23 +0530

    DSA

--- 06-Recursion/ArrayReverse.java
+++ 06-Recursion/ArrayReverse.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class ArrayReverse {
 
     static int[] reverse(int i, int j, int[] arr) {
@@ -12,19 +13,19 @@ public class ArrayReverse {
             reverse(i + 1, j - 1, arr);
         }
         return arr;
-        
+
     }
 
     public static void main(String[] args) {
 
-       Scanner sc =new Scanner(System.in);
-       int[] arr= new int[5];
-       for(int m=0;m<5;m++){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for (int m = 0; m < 5; m++) {
 
-         arr[m] = sc.nextInt();
-       }
+            arr[m] = sc.nextInt();
+        }
 
-       int i = 0, j = (arr.length) - 1;
+        int i = 0, j = (arr.length) - 1;
         reverse(i, j, arr);
         System.out.println("output");
         for (int k = 0; k <= j; k++) {
