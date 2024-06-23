d4a0006b503e23193dd62eb4ce829c2f9dbccd17
Parvathaneni-Tagur <parvathaneni.tagur@gmail.com>
2024-06-23 05:45:12 +0000

    DSA

--- 03-Arrays/Array.java
+++ 03-Arrays/Array.java
@@ -5,6 +5,8 @@ public class Array {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
+
+            System.out.println(sum);
         }
         System.out.println(sum);
     }
